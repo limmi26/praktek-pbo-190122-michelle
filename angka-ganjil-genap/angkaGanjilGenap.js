c4df5afcf169637f26a60a8d1fb3271d8b3e7c5f
limmi26 <kisamakiazuel@gmail.com>
2022-01-24 08:12:05 +0700

    perubahan 2

--- angka-ganjil-genap/angkaGanjilGenap.js
+++ angka-ganjil-genap/angkaGanjilGenap.js
@@ -3,16 +3,28 @@ console.log("#1");
 console.log("###");
 
 for(let i=1; i<=100; i+=3){
-    if(i % 2 === 0){
-        console.log(i+"-genap")
+    if(i % 2 == 0){
+        console.log(i+ "-genap")
     }
-    if(i % 2 !== 0){
-        console.log(i+"-ganjil")
+    if(i % 2 != 0){
+        console.log(i+ "-ganjil")
     }
 }
 
 console.log("#2");
 console.log("###");
 
+for(let i=50; i<=200; i+=5){
+    if(i % 3 == 0){
+        console.log(i+ "-hasil bagi 3");
+    }
+}
+
 console.log("#3");
 console.log("###");
+
+for(let i=100; i<=200; i+=7){
+    if(i % 8 == 0){
+        console.log(i+ "-hasil dibagi")
+    }
+}
